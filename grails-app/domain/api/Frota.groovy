a7bdc4ba8b1f4fb7123541182b8b06a42d3a5ea4
fnascimento <felipegonsalves168@gmail.com>
2023-07-26 16:14:11 -0300

    26/07

--- grails-app/domain/api/Frota.groovy
+++ grails-app/domain/api/Frota.groovy
@@ -3,14 +3,11 @@ import grails.rest.Resource
 
 @Resource(uri='/frota', formats=['json', 'xml'])
 class Frota {
-    int id
     String nome 
-      
-    static hasMany = [lista_navios: Navio]
+     
+    static hasMany = [navios: Navio]
 
     static constraints = {
-        nome blank : false
-        id blank : false     
-        lista_navios blank: true   
+        nome blank : false 
     }
 }
--- grails-app/domain/api/Navio.groovy
+++ grails-app/domain/api/Navio.groovy
@@ -3,11 +3,16 @@ package api
 import grails.rest.Resource
 @Resource(uri='/navio', formats=['json', 'xml'])
 
-class Navio {
+ class Navio {
  Double valor_atual
  String nome
 
+    static hasMany = [frotas: Frota]
+    static belongsTo = [Frota]
+    
+
    static constraints = {
         valor_atual matches: /^(\d+\.?\d*|\.\d+)$/
-    }     
+        
+    }      
 }
